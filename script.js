d60df2d0d9acf775bafb8fa3ffc1d161999f0db5
dasbobbit <bcrossley95@gmail.com>
2021-06-04 17:14:26 +0100

    initial design set up

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log('hi')
+
+let theme = localStorage.getItem("theme")
+
+if (theme == null) {
+    setTheme("light")
+} else {
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName("theme-dot")
+
+
+for (let i=0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener("click", function() {
+        let mode = this.dataset.mode
+        console.log(mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode) {
+    if(mode == "light") {
+        document.getElementById("theme-style").href = "styles/style.css"
+    }
+
+    if(mode == "blue") {
+        document.getElementById("theme-style").href = "styles/blue.css"
+    }
+
+    if(mode == "green") {
+        document.getElementById("theme-style").href = "styles/green.css"
+    }
+
+    if(mode == "purple") {
+        document.getElementById("theme-style").href = "styles/purple.css"
+    }
+
+    localStorage.setItem("theme", mode) 
+
+}
